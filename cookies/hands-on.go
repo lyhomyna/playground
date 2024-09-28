228a8e3c798776362af16a25866a36916823a3b5
lyhomyna <maximligomina011@gmail.com>
2024-09-28 21:25:44 +0300

    reset counter cookie

--- cookies/hands-on.go
+++ cookies/hands-on.go
@@ -10,7 +10,8 @@ import (
 
 func main() {
 	http.HandleFunc("/", rootHandler)
-
+	http.HandleFunc("/hello-again", helloAgainHandler)
+	http.HandleFunc("/delete-cookie", deleteCookie)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 
 	log.Println("server is running")
@@ -18,22 +19,64 @@ func main() {
 }
 
 func rootHandler(w http.ResponseWriter, req *http.Request) {
+	_, err := req.Cookie("visit-counter")
+	if err != nil {
+		// counter cookie isn't present, so need to write it
+		cookie := &http.Cookie{
+			Name:  "visit-counter",
+			Value: "1",
+		}
+		http.SetCookie(w, cookie)
+
+		w.Header().Set("content-type", "text/html")
+		io.WriteString(w, fmt.Sprintf("You have visites site for %s times.", cookie.Value))
+	} else {
+		// counter cookie is present - redirect to hello again page
+		http.Redirect(w, req, "/hello-again", http.StatusSeeOther)
+	}
+}
+
+func updateVisitCounterCookie(req *http.Request) (*http.Cookie, error) {
 	cookie, err := req.Cookie("visit-counter")
 	if err != nil {
-		cookie = &http.Cookie{
-			Name:  "visit-counter",
-			Value: "0",
-		}
+		return nil, err
 	}
 
 	visitCounter, err := strconv.Atoi(cookie.Value)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
 	visitCounter += 1
 	cookie.Value = strconv.Itoa(visitCounter)
 
+	return cookie, nil
+}
+
+func helloAgainHandler(w http.ResponseWriter, req *http.Request) {
+	cookie, err := updateVisitCounterCookie(req)
+	if err != nil {
+		// there is not counter cookie - return to first page
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	w.Header().Set("content-type", "text/html")
 	http.SetCookie(w, cookie)
-	io.WriteString(w, fmt.Sprintf("You have visited this site by %d time.", visitCounter))
+	io.WriteString(w, fmt.Sprintf("You have visites site for %s times. <a href='/delete-cookie'>Reset</a>", cookie.Value))
+}
+
+func deleteCookie(w http.ResponseWriter, req *http.Request) {
+	cookie, err := req.Cookie("visit-counter")
+	if err != nil {
+		// there is not counter cookie - return to first page
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	// delete cookie
+	cookie.MaxAge = -1
+
+	http.SetCookie(w, cookie)
+	http.Redirect(w, req, "/", http.StatusSeeOther)
 }
