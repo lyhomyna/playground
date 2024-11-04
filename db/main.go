c0d0da701e2343afbaf18c228b2472e972a70938
lyhomyna <maximligomina011@gmail.com>
2024-11-04 16:31:17 +0200

    sql queries

--- db/main.go
+++ db/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -21,24 +22,45 @@ type Config struct {
 }
 
 func main() {
-    config, err := loadConfig()
+    log.SetFlags(log.LstdFlags)
+
+    db, err := getDb()
     if err != nil {
 	log.Fatal(err)
     }
-
-    dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", config.Db.User, config.Db.Password, config.Db.Host, config.Db.Port, config.Db.Name)
-
-    db, err := sql.Open("mysql", dsn)
-    if err != nil {
-	log.Fatal("Failed to connect to database.")
-    }
     defer db.Close()
 
-    if err := db.Ping(); err != nil {
-	log.Fatal("Failed to ping database.")
+    _, err = db.Query("CREATE TABLE IF NOT EXISTS users(id INT NOT NULL AUTO_INCREMENT, name VARCHAR(20), PRIMARY KEY (id));")
+    if err != nil {
+        log.Fatalf("Failed CREATE TABLE. %+v", err)
     }
 
-    log.Println("Successfully conected to db.")
+    _, err = db.Query("INSERT INTO users (name) VALUES ('Sam'), ('Mike'), ('Marek');")
+    if err != nil {
+        log.Fatalf("Failed INSERT values. %+v", err)
+    }
+
+    rows, err := db.Query("SELECT * FROM users;")
+    if err != nil {
+        log.Fatalf("Failed SELECT. %+v", err)
+    }
+
+    for rows.Next() {
+	var id int
+	var name string
+
+	err = rows.Scan(&id, &name)
+	if err != nil {
+	    log.Fatal(err)
+	}
+
+	log.Printf("Id: %d\tName: %s", id, name)
+    }
+
+    _, err = db.Query("TRUNCATE TABLE users")
+    if err != nil {
+        log.Fatalf("Failed TRUNCATE TABLE. %+v", err)
+    }
 }
 
 func loadConfig() (*Config, error) {
@@ -55,4 +77,25 @@ func loadConfig() (*Config, error) {
     }
 
     return &config, nil
+}
+
+func getDb() (*sql.DB, error) {
+    config, err := loadConfig()
+    if err != nil {
+	return nil, errors.New("Failed to load config.") 
+    }
+
+    dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", config.Db.User, config.Db.Password, config.Db.Host, config.Db.Port, config.Db.Name)
+
+    db, err := sql.Open("mysql", dsn)
+    if err != nil {
+	return nil, errors.New("Failed to connect to db.") 
+    }
+
+    if err := db.Ping(); err != nil {
+	return nil, errors.New("Failed to ping db.")
+    }
+
+    log.Println("Successfully conected to db.")
+    return db, nil
 }
